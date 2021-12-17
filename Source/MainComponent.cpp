d6090187ddd6fba26b8fac02cd67799970c6784a
Ronald Mo <ronaldkmo@gmail.com>
2021-12-17 13:07:00 +0800

    Update MainComponent.cpp

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -42,7 +42,7 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
         Main idea: use a for loop to calculate the audio samples and send them to the output buffer. In the loop, you should:
          1. Calculate y(.) for the current sample. That is, amplitude * sin(angle).
              Store the value into a new float variable currentSample.
-             > function std::sin() can be used to calculate sin().
+             > function std::sin() is used to calculate sin().
              > currentAngle stores the angle for the current sample.
              > amplitude stores the amplitude.
             
